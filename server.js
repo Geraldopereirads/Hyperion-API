68ccc57bf58e3a195d20170129f2e611e15dbe4a
Geraldo Pereira <110185110+Geraldopereirads@users.noreply.github.com>
2024-01-20 11:52:24 -0300

    fix: repair pachege-lock.json

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 const axios = require("axios");
-import cors from "cors";
-import { router as _router, create } from "json-server";
-import auth, { rewriter } from "json-server-auth";
+const cors = require("cors");
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 
-const port = 3000;
+const port = 3001;
 
-const app = create();
-const router = _router("db.json");
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
 
 app.db = router.db;
 
 async function main() {
-  const acessUrl = async () => {
+  const accessUrl = async () => {
     try {
       await axios.get('https://hyperion-r5lkcwvyd-geraldopereirads.vercel.app/gameStore');
     } catch (error) {
       console.error("Error fetching data:", error);
-      setInterval(acessUrl, 5000);
+      setInterval(accessUrl, 5000);
     }
   };
 
-  setInterval(acessUrl, 10000);
+  setInterval(accessUrl, 10000);
 }
 
-main()
+main();
 
-const rules = rewriter({
+const rules = auth.rewriter({
   users: 600,
 });
 
@@ -33,12 +33,8 @@ app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
-app.listen(port);
+app.listen(port, () => {
+  console.log("Server is running on port:", port);
+});
 
 main();
-
-
-
-console.log("Server is running on port:", port);
-
-/* A senha do Kenzinho é 123456 */
