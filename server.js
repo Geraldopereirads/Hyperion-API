ff17917350143a34555e3539f7b223a085220346
Geraldo Pereira <110185110+Geraldopereirads@users.noreply.github.com>
2024-01-20 12:16:17 -0300

    fix: repair pachege-lock.json0

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const axios = require("axios");
-const cors = require("cors");
-const jsonServer = require("json-server");
-const auth = require("json-server-auth");
-
+import axios from "axios";
+import cors from "cors";
+import jsonServer from "json-server";
+import auth from "json-server-auth";
+const { create, rewriter } = jsonServer;
 const port = 3001;
 
-const app = jsonServer.create();
-const router = jsonServer.router("db.json");
+const app = create();
+const router = create();
+const rules = rewriter({
+  users: 600,
+});
 
 app.db = router.db;
 
@@ -18,23 +21,18 @@ async function main() {
       console.error("Error fetching data:", error);
       setInterval(accessUrl, 5000);
     }
-  };
+  }
 
   setInterval(accessUrl, 10000);
 }
 
 main();
 
-const rules = auth.rewriter({
-  users: 600,
-});
-
 app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
+
 app.listen(port, () => {
   console.log("Server is running on port:", port);
 });
-
-main();
